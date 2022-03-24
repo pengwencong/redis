fcdcaa85cb1d8bcebca4613c3cec3ee4a516eff8
pengewncong <185064212@qq.com>
2022-03-24 12:09:24 +0800

    master

---
+++ redis.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#docker pull redis:latest
+
+#git clone git@github.com:pengwencong/redis.git
+
+#chmod 777 -R redis
+
+#docker run -it --name redis -p 6379:6379  -v /home/redis/conf/redis.conf:/usr/share/elasticsearch/config/elasticsearch.yml -v /home/redis/data:/usr/share/redis/data -d redis:latest
