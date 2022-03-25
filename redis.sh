8e32dbec73d0b24d03f9e1845c74f5acdf9a3413
pengewncong <185064212@qq.com>
2022-03-25 11:22:46 +0800

    re

--- redis.sh
+++ redis.sh
@@ -6,4 +6,4 @@ git clone git@github.com:pengwencong/redis.git
 
 chmod 777 -R redis
 
-docker run -it --name redis -p 6379:6379 -p 16379:16379 -v /home/redis/conf/redis.conf:/usr/local/etc/redis/redis.conf -v /home/redis/data:/data -d redis:latest redis-server /usr/local/etc/redis/redis.conf
+docker run -it --name redis --net host -p 6379 -p 16379 -v /home/redis/conf/redis.conf:/usr/local/etc/redis/redis.conf -v /home/redis/data:/data -d redis:latest redis-server /usr/local/etc/redis/redis.conf
